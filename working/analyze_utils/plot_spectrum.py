6cb085582ef9348b492cc471356d79faeaeb69a0
Ruiqi <rfeng6@ncsu.edu>
2023-04-10 17:56:36 -0400

    [Exp] add results of landscape.
    Can be used in: measurement; reverse engineering
    In higher dimension, a perturbation of a certain l_1
    distance (~10^2 nm) would fall in the same local minimum

--- working/analyze_utils/plot_spectrum.py
+++ working/analyze_utils/plot_spectrum.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from spectrum import SpectrumSimple
 
-def plot_spectrum(s: SpectrumSimple, R=True, T=False):
+
+def plot_spectrum(s: SpectrumSimple, R=True, T=False, ax=None, fig=None):
     wls = s.WLS
     assert R or T, "you have to plot something..."
     if R and T:
-        fig, ax = plt.subplots(1, 2)
+        if ax is None or fig is None:
+            fig, ax = plt.subplots(1, 2)
         fig.tight_layout()
 
         R_arr = s.get_R()
@@ -18,22 +20,23 @@ def plot_spectrum(s: SpectrumSimple, R=True, T=False):
         ax[1].set_xlabel('wl / nm')
         ax[0].set_title('R')
         ax[1].set_title('T')
-        
+
         ax[0].set_xlim(np.min(wls), np.max(wls))
         ax[1].set_xlim(np.min(wls), np.max(wls))
         ax[0].set_ylim(0., 1.)
         ax[1].set_ylim(0., 1.)
-        
+
     else:
-        fig, ax = plt.subplots(1, 1)
+        if ax is None:
+            fig, ax = plt.subplots(1, 1)
         if R:
             ax.plot(wls, s.get_R(), label='R')
         else:
             ax.plot(wls, s.get_T(), label='T')
-        
+
         ax.set_xlabel('wl / nm')
         title = "R" if R else "T"
         ax.set_title(title)
         ax.set_xlim(np.min(wls), np.max(wls))
         ax.set_ylim(0., 1.)
-    return fig, ax
+    return ax

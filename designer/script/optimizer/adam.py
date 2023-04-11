609c59313876e7d97ef25843785ef441ba5b756e
Ruiqi Feng <rfeng6@ncsu.edu>
2023-04-11 00:18:34 -0400

    [Implementation] implemented SGD & helper J/f
    Some performance decrease was observed when minibatch
    approaches the size of the complete array.

--- designer/script/optimizer/adam.py
+++ designer/script/optimizer/adam.py
@@ -1,11 +1,12 @@
+import sys
+sys.path.append('./designer/script/')
+
 from optimizer.grad_helper import stack_f, stack_J, stack_init_params
 from utils.loss import calculate_RMS_f_spec
 from spectrum import BaseSpectrum
 from film import FilmSimple
 import numpy as np
-from numpy.random import Generator as rng
-import sys
-sys.path.append('./designer/script/')
+
 
 
 def adam_optimize(
@@ -27,17 +28,27 @@ def adam_optimize(
 
     Adapted from Kingma, Diederik P. and Jimmy Ba.
      "Adam: A Method for Stochastic Optimization." CoRR abs/1412.6980 (2014)
+
+    Parameters:
+        batch_size_wl: 
+            size of each wl mini-batch. for every wl, R and T are 
+            both calculated. Therefore, J and f have size (in the 
+            corresponding axis): batch_size_wl.shape[0] * 2.
     '''
 
     # check batch size
-    wl_num_min = np.min([s.WLS.shape[0] * 2 for s in target_spec_ls])
+    wl_num_min = np.min([s.WLS.shape[0] for s in target_spec_ls])
+    if batch_size_spec is None:
+        batch_size_spec = len(target_spec_ls)
+    if batch_size_wl is None:
+        batch_size_wl = wl_num_min
     assert batch_size_spec <= len(target_spec_ls) \
         and batch_size_wl <= wl_num_min # spec with smallest wl
 
     # Prep: calculate refractive index & stack target spectrum into one array
     n_arrs_ls = stack_init_params(film, target_spec_ls)
     d = film.get_d()
-    total_wl_num = batch_size_wl * batch_size_spec # R & T
+    total_wl_num = batch_size_wl * batch_size_spec * 2 # R & T
     J = np.empty((total_wl_num, d.shape[0]))  
     f = np.empty(total_wl_num)
 
@@ -49,18 +60,23 @@ def adam_optimize(
 
     for t in range(max_steps):
         # shuffle for sgd
-        spec_batch_idx = rng.choice(
-            batch_size_spec, 
+        spec_batch_idx = np.random.default_rng().choice(
             len(target_spec_ls), 
+            batch_size_spec, 
             replace=False
         )
         spec_batch_idx = np.sort(spec_batch_idx)
         # shuffle R and T. 
-        # mat: 2 #wls \cross #spec; pick out elem on the crossing of 
-        # rows=wl_idx and cols=spec_idx 
-        wl_batch_idx = rng.choice(
-            batch_size_wl, 
+        # mat: #wls \cross #spec; pick out elem on the crossing of 
+        # rows=wl_idx and cols=spec_idx. For selected wl, R and T
+        # are calculated simultaneously. 
+        # 
+        # The size of J is fixed but
+        # the stored grads are different in each epoch according to 
+        # the random shuffle.
+        wl_batch_idx = np.random.default_rng().choice( 
             wl_num_min, 
+            batch_size_wl,
             replace=False
         )
         wl_batch_idx = np.sort(wl_batch_idx)
@@ -71,7 +87,7 @@ def adam_optimize(
             d, 
             target_spec_ls, 
             MAX_LAYER_NUMBER=250, 
-            batch_idx = spec_batch_idx, 
+            spec_batch_idx = spec_batch_idx, 
             wl_batch_idx = wl_batch_idx
         )
         stack_f(
@@ -113,60 +129,4 @@ def adam_optimize(
     if record:
         return losses
 
-    return None
-
-def adam_optimize_non_sgd(
-        film: FilmSimple,
-        target_spec_ls: list[BaseSpectrum], 
-        max_steps, 
-        alpha=0.001, # stepsize TODO: fine tune this...
-        beta1 = 0.9, 
-        beta2 = 0.999, 
-        epsilon = 1e-8, 
-        record = False, 
-        show=False
-    ):
-    # Adapted from Kingma, Diederik P. and Jimmy Ba. 
-    # "Adam: A Method for Stochastic Optimization." CoRR abs/1412.6980 (2014)
-
-    # Prep: calculate refractive index & stack target spectrum into one array
-
-    target_spec, n_arrs_ls = stack_init_params(film, target_spec_ls)
-    d = film.get_d()
-    J = np.empty((target_spec.shape[0], d.shape[0]))
-    f = np.empty(target_spec.shape[0])
-    losses = []
-    # initialize
-    m = 0
-    v = 0
-
-    for t in range(max_steps):
-        stack_J(J, n_arrs_ls, d, target_spec_ls, MAX_LAYER_NUMBER=250)
-        stack_f(f, n_arrs_ls, d, target_spec_ls, target_spec)
-
-        g = J.T @ f
-        m = beta1 * m + (1 - beta1) * g
-        v = beta2 * v + (1 - beta2) * g ** 2
-        m_hat = m  / (1 - beta1)
-        v_hat = v / (1 - beta2)
-        d = d - alpha * m_hat / (np.sqrt(v_hat) + epsilon)
-        # Project back to feasible domain
-        d[d < 0] = 0.
-        if record:
-            losses.append(calculate_RMS_f_spec(film, target_spec_ls))
-        if show:
-            print(f'iter {t}, loss {calculate_RMS_f_spec(film, target_spec_ls)}')
-        film.update_d(d)    
-        # if loss not decreasing, break
-        try:
-            if losses[-1] == losses[-2]:
-
-                if np.array_equal(losses[-10:], [losses[-1]] * 10):
-                    print('convergent, terminate eraly')
-                    break
-        except Exception as e:
-            continue
-
-    if record:
-        return losses
     return None

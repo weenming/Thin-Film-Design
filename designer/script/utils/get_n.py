fd32137b711e5a35ad691ea03fed025197cedaf7
Ruiqi Feng <rfeng6@ncsu.edu>
2023-04-26 15:51:30 -0400

    [Exp] add new free form reverse eng

--- designer/script/utils/get_n.py
+++ designer/script/utils/get_n.py
@@ -25,8 +25,13 @@ def get_n_1(wl):
     return wl / wl  # broadcast if is instance of np.array
 
 
+def get_n_1_5(wl):
+    return 1.5 * wl / wl
+
+
 def get_n_2(wl):
     return 2 * wl / wl
+
 
 def get_n_free(wl, n: complex):
     return wl / wl * n

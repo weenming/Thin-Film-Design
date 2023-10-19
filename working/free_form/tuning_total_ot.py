0cd0b5d5f7eee55836e29f1b7cf9d790afeb804f
rq <rqfeng20@fudan.edu.cn>
2023-10-19 23:37:16 +0800

    add more tuning exp

--- working/free_form/tuning_total_ot.py
+++ working/free_form/tuning_total_ot.py
@@ -1,9 +1,10 @@
+# %%
+import os
 import sys
-sys.path.append('./working')
-sys.path.append('./designer/')
-sys.path.append('./designer/script/')
+sys.path.append(os.path.dirname(__file__) + '/../../')
+sys.path.append(os.path.dirname(__file__) + '/..')
+sys.path.append(os.path.dirname(__file__) + '/../../designer/script/')
 
-import os 
 
 from film import FreeFormFilm, BaseFilm, TwoMaterialFilm, EqOTFilm
 from design import BaseDesign
@@ -22,7 +23,6 @@ import matplotlib.patches as mpatches
 import copy
 import pickle
 
-
 # design = make_edgefilter_design()
 # design = make_reflection_design(wls=np.linspace(695, 939, 500))
 reps = 1
@@ -31,21 +31,22 @@ n_min = 1.5
 n_max = 3
 
 # adam optimizer parameters
-alpha = 1e-2
+alpha = 1
 batch_size = None
-max_steps=500
+max_steps=1000
 show = False
+
 
 def exp(n_size, each_ot, target):
     np.random.seed()
-    init_n = np.random.random(n_size) + 1.5
-    init_film = EqOTFilm(init_n, each_ot * n_size, substrate=1.5)
+    init_n = np.zeros(n_size) + 2
+    film = EqOTFilm(init_n, each_ot * n_size, substrate=1.5)
 
     adam_optimizer = AdamFreeFormOptimizer(
-        init_film, 
+        film, 
         target, 
         max_steps=max_steps, 
-        alpha=alpha, # 0.01 for big OT films...
+        alpha=alpha * 1e2 / (n_size * each_ot), # 0.01 for big OT films...
         record=True, 
         show=show, 
         n_min=n_min, # 1.5
@@ -54,18 +55,34 @@ def exp(n_size, each_ot, target):
     )
 
     adam_optimizer.optimize()
-    return calculate_RMS_f_spec(init_film, target), init_film
+    return calculate_RMS_f_spec(film, target), film
 
-each_ots = np.array([i for i in 10 ** np.linspace(-1, 3, 50)])
-n_sizes = np.array([int(i) for i in 10 ** np.linspace(0, 4, 50)])
 
 # wl_min = 500
 # wl_max = 1000 # 1 / wl_max - 1 / wl_min = 1 / 1000
 
 # target = make_triband_filter_design().target_specs # 1 / wl_max - 1 / wl_min = 
-target = [get_minus_filter_spec(np.linspace(400., 700., 300))]
+wls = np.linspace(400., 700., 500)
+target = [get_minus_filter_spec(wls)]
 target_name = 'minus_filter'
 
+# %%
+# test
+loss, film = exp(1000, 1000, target) # this is sufficient for spectrum under-sampling
+
+# %%
+wls_dense = np.linspace(wls[0], wls[-1], 1000)
+fig, ax = plt.subplots(1, 1)
+ax.plot(wls_dense, film.get_spec(0., wls_dense).get_R(), label='design')
+ax.scatter(wls, target[0].get_R(), label='target', c='C1')
+
+ax.legend()
+ax.set_ylabel('R')
+ax.set_xlabel('wl')
+
+# %%
+each_ots = np.array([i for i in 10 ** np.linspace(-1, 3, 50)])
+n_sizes = np.array([int(i) for i in 10 ** np.linspace(0, 3, 50)])
 
 
 films_arr_rep, best_loss_arr_rep, best_film_arr_rep = [], [], []
@@ -85,4 +102,6 @@ for rep in range(reps):
             best_film_arr_rep[-1][-1].append(best_film)
 print(best_loss_arr_rep)
 
-save(os.path.dirname(__file__) + f'/raw_result_sync/free_form_params_{target_name}_ot1e-1to1e3_nsize1to1e4.5_log_int_large_n_size', np.array(best_loss_arr_rep), np.array(best_film_arr_rep))
+save(os.path.dirname(__file__) + f'/raw_result_total_ot/free_form_params_{target_name}_target_sparse', np.array(best_loss_arr_rep), np.array(best_film_arr_rep))
+
+
